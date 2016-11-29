a7459ab252f77d1d5e9197ee24f13ed950761f0c
Darrenf <darrenf54321@gmail.com>
2016-11-29 10:13:54 +0000

    failing test for making a deposit

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -4,10 +4,16 @@ require 'bank_account'
 describe BankAccount do
   subject(:account) { BankAccount.new }
 
-describe 'opening balance' do
+
   it 'has an opening balance of zero' do
     expect(account.show_balance).to eq(0)
   end
-end
+
+
+  it 'can make a deposit' do
+    account.deposit(150)
+    expect(account.show_balance).to eq 150
+  end
+
 
 end
